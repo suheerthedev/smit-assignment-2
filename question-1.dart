49c2975821ac4fcabdc45b98fa83302e493bc2bf
suheerthedev <khansuheer1@gmail.com>
2024-07-23 18:24:11 +0500

    Updated Answer 1

--- question-1.dart
+++ question-1.dart
@@ -1,8 +1,39 @@
 //Q.1: Create a list of names and print all names using list.
 
 void main() {
-  List names = ["Suheer", "Abdullah", "Ahad", "Mohammad"];
+  List names = ["Suheer", "Abdullah", "Ahad", "Zubair", "Mohammad"];
   for (int i = 0; i < names.length; i++) {
     print(names[i]);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//   List names = ["Suheer", "Abdullah", "Ahad", "Mohammad"];
+//   for (int i = 0; i < names.length; i++) {
+//     print(names[i]);
+//   }
+// }
