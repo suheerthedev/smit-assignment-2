7bc5333cc11f41b8d51306764c3d4a65ff4826ca
suheerthedev <khansuheer1@gmail.com>
2024-07-24 21:21:59 +0500

    Updated Answer 20

--- question-20.dart
+++ question-20.dart
@@ -1,18 +1,49 @@
 // Q.20: Create a map named "car" with the following key-value pairs: "brand" as "Toyota", "color" as "Red", "isSedan" as true. Write Dart code to check if the car is a sedan and red in color. Print "Match" if both conditions are true, otherwise print "No match".
 
 void main() {
-  Map<String, dynamic> car = {
-    'brand': 'Toyota',
-    'color': 'Red',
-    'isSedan': true,
-  };
+  Map car = {'brand': 'Toyota', 'color': 'red', 'isSedan': true};
 
-  bool isSedan = car['isSedan'];
-  String color = car['color'];
-
-  if (isSedan && color == 'Red') {
+  if (car['isSedan'] == true && car['color'] == 'red') {
     print('Match');
   } else {
-    print('No match');
+    print("No Match");
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//   Map<String, dynamic> car = {
+//     'brand': 'Toyota',
+//     'color': 'Red',
+//     'isSedan': true,
+//   };
+
+//   bool isSedan = car['isSedan'];
+//   String color = car['color'];
+
+//   if (isSedan && color == 'Red') {
+//     print('Match');
+//   } else {
+//     print('No match');
+//   }
+// }
