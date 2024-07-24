6e0b6cc03f361297a7fdecb2eafda5c24ae2aeea
suheerthedev <khansuheer1@gmail.com>
2024-07-24 06:29:31 +0500

    Updated Answer 8

--- question-8.dart
+++ question-8.dart
@@ -1,16 +1,47 @@
 // Q.8: Remove all false values from below list by using removeWhere or retainWhere property.
 
-void main() {
-  List<Map<dynamic, dynamic>> usersEligibility = [
-    {'name': 'John', 'eligible': true},
-    {'name': 'Alice', 'eligible': false},
-    {'name': 'Mike', 'eligible': true},
-    {'name': 'Sarah', 'eligible': true},
-    {'name': 'Tom', 'eligible': false},
-  ];
 
-  // Using removeWhere to remove all entries with 'eligible' set to false
-  usersEligibility.removeWhere((user) => user['eligible'] == false);
 
-  print(usersEligibility);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//   List<Map<dynamic, dynamic>> usersEligibility = [
+//     {'name': 'John', 'eligible': true},
+//     {'name': 'Alice', 'eligible': false},
+//     {'name': 'Mike', 'eligible': true},
+//     {'name': 'Sarah', 'eligible': true},
+//     {'name': 'Tom', 'eligible': false},
+//   ];
+
+//   // Using removeWhere to remove all entries with 'eligible' set to false
+//   usersEligibility.removeWhere((user) => user['eligible'] == false);
+
+//   print(usersEligibility);
+// }
