ccb1eb14b3a330b0d1d592d24cbb650962aad3c9
suheerthedev <khansuheer1@gmail.com>
2024-07-24 07:17:00 +0500

    Updated Answer 12

--- question-12.dart
+++ question-12.dart
@@ -1,11 +1,9 @@
 // Q.12: Write a Dart code that takes in a list of strings and prints a new list with the elements in reverse order. The original list should remain unchanged.
 
 void main() {
-  List<String> originalList = ['apple', 'banana', 'cherry', 'peach', 'mango'];
-
-  // Create a new list with elements in reverse order
-  List<String> reversedList = List.from(originalList.reversed);
+  List originalList = ["a", "b", "c", "d"];
+  List newList = originalList.reversed.toList();
 
   print("Original List: $originalList");
-  print("Reversed List: $reversedList");
+  print("New Reversed List: $newList");
 }
