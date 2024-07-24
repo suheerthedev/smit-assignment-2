c8271b31f84fedc421fe79cf0eaaa39a0549f5f5
suheerthedev <khansuheer1@gmail.com>
2024-07-24 19:04:28 +0500

    Updated Answer 5

--- question-5.dart
+++ question-5.dart
@@ -1,6 +1,10 @@
 // Q.5: Create a map with name, phone keys and store some values to it. Use where to find all keys that have length 4.
 
+void main() {
+  Map map = {'name': 'suheer', 'phone': '012'};
 
+  map.
+}
 
 
 
