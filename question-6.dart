b5b2fb0caa3a704ab742117a77c0276f906413fd
suheerthedev <khansuheer1@gmail.com>
2024-07-24 19:24:25 +0500

    Updated Answer 6

--- question-6.dart
+++ question-6.dart
@@ -3,33 +3,83 @@
 import 'dart:io';
 
 void main() {
-  Map<String, Map<String, String>> world = {
-    'Pakistan': {
-      'capitalCity': 'Islamabad',
-      'currency': 'PKR',
-      'language': 'Urdu'
-    },
-    'India': {
-      'capitalCity': 'New Dehli',
-      'currency': 'INR',
-      'language': 'Hindi'
-    },
-    'USA': {
-      'capitalCity': 'Washington',
-      'currency': 'USD',
-      'language': 'English'
-    }
+  Map world = {
+    'Pakistan': {'capitalCity': 'Islamabad', 'currency': 'PKR', 'lang': 'urdu'},
+    'USA': {'capitalCity': 'Washington', 'currency': 'USD', 'lang': 'english'},
+    'India': {'capitalCity': 'New Dehli', 'currency': 'INR', 'lang': 'hindi'}
   };
-  print("Enter the Country Name");
-  String countryKey = stdin.readLineSync()!;
-  // String countryKey = 'Pakistan';
+  print("Enter Country Name (Pakistan/USA/India)");
+  var countryName = stdin.readLineSync();
 
-  if (world.containsKey(countryKey)) {
-    Map<String, String> countryInfo = world[countryKey]!;
-    print('Country: $countryKey');
-    print('Capital City: ${countryInfo['capitalCity']}');
-    print('Currency: ${countryInfo['currency']}');
-  } else {
-    print('Country $countryKey not found in the world map.');
+  if (world.containsKey(countryName)) {
+    print("Capital City Is: ${world['$countryName']['capitalCity']}");
+    print("Currency Is: ${world['$countryName']['currency']}");
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // while (!(world.containsKey(countryName))) {
+  //   if (world.containsKey(countryName)) {
+  //     print("Capital City Is: ${world['$countryName']['capitalCity']}");
+  //     print("Currency Is: ${world['$countryName']['currency']}");
+  //   } else {
+  //     print("Wrong Entry!");
+  //     print("Try Again");
+  //     print("");
+  //     print("Enter Country Name (Pakistan/USA/India)");
+  //     var countryName = stdin.readLineSync();
+  //   }
+  //   break;
+  // }
+
+
+
+
+
+
+
+
+
+// void main() {
+//   Map<String, Map<String, String>> world = {
+//     'Pakistan': {
+//       'capitalCity': 'Islamabad',
+//       'currency': 'PKR',
+//       'language': 'Urdu'
+//     },
+//     'India': {
+//       'capitalCity': 'New Dehli',
+//       'currency': 'INR',
+//       'language': 'Hindi'
+//     },
+//     'USA': {
+//       'capitalCity': 'Washington',
+//       'currency': 'USD',
+//       'language': 'English'
+//     }
+//   };
+//   print("Enter the Country Name");
+//   String countryKey = stdin.readLineSync()!;
+//   // String countryKey = 'Pakistan';
+
+//   if (world.containsKey(countryKey)) {
+//     Map<String, String> countryInfo = world[countryKey]!;
+//     print('Country: $countryKey');
+//     print('Capital City: ${countryInfo['capitalCity']}');
+//     print('Currency: ${countryInfo['currency']}');
+//   } else {
+//     print('Country $countryKey not found in the world map.');
+//   }
+// }
