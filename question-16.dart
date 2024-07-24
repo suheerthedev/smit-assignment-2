7eff29701898840cbd5fc8f33171722db24f93f5
suheerthedev <khansuheer1@gmail.com>
2024-07-24 08:45:25 +0500

    Updated Answer 16

--- question-16.dart
+++ question-16.dart
@@ -1,11 +1,17 @@
 // Q.16: Implement a Dart code that uses the where() method to filter out odd numbers from a list of integers. The program should take in the original list as a parameter and print a new list containing only the even numbers.
 
-void main() {
-  List<int> originalList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// void main() {
+//   List originalList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   List newlist = originalList.where((element) => element.isEven).toList();
 
-  List<int> evenNumbers =
-      originalList.where((int number) => number % 2 == 0).toList();
+//   print("Original List: $originalList");
+//   print("New List: $newlist");
+// }
+
+void main() {
+  List originalList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  List newlist = originalList.where((element) => element % 2 == 0).toList();
 
   print("Original List: $originalList");
-  print("Even Numbers: $evenNumbers");
+  print("New List: $newlist");
 }
