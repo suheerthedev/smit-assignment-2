a3ebeb0442da324fc703b646f5cd31d67458016a
suheerthedev <khansuheer1@gmail.com>
2024-07-24 07:12:19 +0500

    Updated Answer 11

--- question-11.dart
+++ question-11.dart
@@ -1,11 +1,13 @@
 // Q.11: Write a Dart code that takes in a list and an integer n as parameters. The program should print a new list containing the first n elements from the original list.
 
 void main() {
-  List<int> originalList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  int n = 2; // Number of elements to take
+  List originalList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  int n = 2;
+  List newList = [];
 
-  List<int> newList = originalList.take(n).toList();
-
+  for (int i = 0; i < n; i++) {
+    newList.add(originalList[i]);
+  }
   print("Original List: $originalList");
-  print("New List with first $n elements: $newList");
+  print("New List with $n Elements: $newList");
 }
