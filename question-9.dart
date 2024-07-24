36235e36a5ebf46c861237d7da4990f577798d6b
suheerthedev <khansuheer1@gmail.com>
2024-07-24 06:27:12 +0500

    Updated Answer 9

--- question-9.dart
+++ question-9.dart
@@ -1,13 +1,49 @@
 // Q.9: Given a list of integers, write a dart code that returns the maximum value from the list.
 
 void main() {
-  List<int> numbers = [1, 5, 10, 105, 25, 20, 30];
+  List integers = [11, 220, 33, 44, 550, 66, 77];
 
-  // Finding the maximum value in the list using the max method
+  int maximumValue = integers[0];
 
-  numbers.sort();
-  print(numbers);
-  var maxValue = numbers.last;
+  for (int i = 0; i < integers.length; i++) {
+    if (integers[i] > maximumValue) {
+      maximumValue = integers[i];
+    }
+  }
 
-  print("The maximum value is: $maxValue");
+  print("The Maximum Value In The List Is: $maximumValue");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//   List<int> numbers = [1, 5, 10, 105, 25, 20, 30];
+
+//   // Finding the maximum value in the list using the max method
+
+//   numbers.sort();
+//   print(numbers);
+//   var maxValue = numbers.last;
+
+//   print("The maximum value is: $maxValue");
+// }
